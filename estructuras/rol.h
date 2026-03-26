e1e2d2986ebc24a3dd2364a47dd0f3c47bdc778b
Nerea Garoña <nerea.garona@opendeusto.es>
2026-03-26 10:43:45 +0100

    crear la carpeta estructuras y el tipo de rol

---
+++ estructuras/rol.h
@@ -0,0 +1,17 @@
+/*
+ * rol.h
+ *
+ *  Created on: 26 mar 2026
+ *      Author: nerea.garona
+ */
+
+#ifndef ROL_H_
+#define ROL_H_
+
+typedef enum {
+	ORGANIZADOR,
+	HACKER
+} TipoRol;
+
+
+#endif /* ROL_H_ */
